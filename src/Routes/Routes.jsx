c65d3b3633921bbd38fdf11c7d91e80004ad1dd7
Levi9111 <captainlevi9111@gmail.com>
2023-06-08 20:27:16 +0600

    Error page created with anime js

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Main from '../Layout/Main/Main';
 import Home from '../Pages/Home/Home/Home';
+import Error from '../Error/Error';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <Main></Main>,
+    errorElement: <Error></Error>,
     children: [
       {
         path: '/',
