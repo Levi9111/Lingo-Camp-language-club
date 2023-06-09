ad3c5b8e529a75648aa9a133d1cf691ce3a0e2da
Levi9111 <captainlevi9111@gmail.com>
2023-06-09 13:03:25 +0600

    popular classes section added

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from '../Banner/Banner';
+import PopularClasses from '../PopularClasses/PopularClasses';
 
 const Home = () => {
   return (
     <section>
       <Banner></Banner>
+      <PopularClasses></PopularClasses>
     </section>
   );
 };
---
+++ src/Pages/Home/PopularClasses/PopularClasses.jsx
@@ -0,0 +1,51 @@
+import './PopularClasses.css';
+import { useEffect, useState } from 'react';
+
+const PopularClasses = () => {
+  const [classes, setClasses] = useState([]);
+  //   const [popularClasses, setPopularClasses] = useState([]);
+  useEffect(() => {
+    fetch('classes.json')
+      .then((res) => res.json())
+      .then((data) => setClasses(data))
+      .catch((err) => console.log(err));
+  }, []);
+  const popularClasses = classes.filter(
+    (languageClass) => languageClass.category === 'popular',
+  );
+
+  console.log(popularClasses);
+
+  return (
+    <section className='md:max-w-[80%] mx-auto'>
+      <h3 className="text-center text-2xl underline italic mb-5 font-semibold text-violet-800">
+        Popular Classes
+      </h3>
+      <div className="grid md:grid-cols-3">
+        {popularClasses.map((popularClass, index) => (
+          <div className="box-wrapper" key={popularClass.id}>
+            <figure className="shape-box shape-box_half">
+              <img src={popularClass.image} alt="" />
+              <div className="brk-abs-overlay z-index-0 bg-black opacity-60"></div>
+              <figcaption>
+                <div className="show-content">
+                  <h3 className="card-no">0{index + 1}</h3>
+                  <h4 className="card-main-title">{popularClass.name}</h4>
+                </div>
+                <div className="card-content">
+                  <h3>Instructor: {popularClass.instructor}</h3>
+                  <p>Total number of Students: {popularClass.numStudents}</p>
+                  <p>Available seats: {popularClass.availableSeats}</p>
+                  <p>Price: ${popularClass.price}</p>
+                </div>
+              </figcaption>
+              <span className="after"></span>
+            </figure>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default PopularClasses;
